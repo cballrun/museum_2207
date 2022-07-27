1bcac693ad241af43e36e05ad2492646fc139164
Carter Ball <carter.k.ball@gmail.com>
2022-07-27 11:43:59 -0400

    Set up test for adding exhibits to Museum class

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -5,6 +5,9 @@ require './lib/museum'
 describe Museum do
   before :each do
     @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
   end
 
   it 'exists' do
@@ -17,6 +20,13 @@ describe Museum do
 
   it 'has exhibits' do
     expect(@dmns.exhibits).to eq([])
+  end
+
+  it 'can add exhibits' do
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
   end
 
 
