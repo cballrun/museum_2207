ad3a9855f50663bd24b101972822c5750a38a7b6
Carter Ball <carter.k.ball@gmail.com>
2022-07-27 12:10:08 -0400

    Move add_exhibit methods to museum_spec :each block, change patron_1 spending money, add patron_3 to :each block

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -8,8 +8,12 @@ describe Museum do
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
-    @patron_1 = Patron.new("Bob", 20)
+    @patron_1 = Patron.new("Bob", 0)
     @patron_2 = Patron.new("Sally", 20)
+    @patron_3 = Patron.new("Johnny", 5)
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
   end
 
   it 'exists' do
@@ -25,22 +29,15 @@ describe Museum do
   end
 
   it 'can add exhibits' do
-    @dmns.add_exhibit(@gems_and_minerals)
-    @dmns.add_exhibit(@dead_sea_scrolls)
-    @dmns.add_exhibit(@imax)
     expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
   end
 
   it 'can recommend exhibits' do
-    @dmns.add_exhibit(@gems_and_minerals)
-    @dmns.add_exhibit(@dead_sea_scrolls)
-    @dmns.add_exhibit(@imax)
     @patron_1.add_interest("Dead Sea Scrolls")
     @patron_1.add_interest("Gems and Minerals")
     @patron_2.add_interest("IMAX")
     
     expect(@dmns.recommend_exhibits(@patron_1)).to eq([@gems_and_minerals, @dead_sea_scrolls])
-    #remember to check order of above
     expect(@dmns.recommend_exhibits(@patron_2)).to eq([@imax])
   end
 
